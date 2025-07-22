b71f9bb70308de5ad7dfa93ac2eff5e854328f39
Priya <priyamathur090@gmail.com>
2025-07-22 20:51:36 +0530

    Telecom

---
+++ src/test/java/contactlist/BaseTest.java
@@ -0,0 +1,17 @@
+package contactlist;
+
+import io.restassured.RestAssured;
+
+public class BaseTest {
+    protected static final String BASE_URL = "https://thinking-tester-contact-list.herokuapp.com";
+    protected static String firstToken;
+    protected static String secondToken;
+    protected static String contactId;
+    
+    protected static String testEmail;
+    protected static final String testPassword = "myPassword";
+
+    static {
+        RestAssured.baseURI = BASE_URL;
+    }
+}
